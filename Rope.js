db140177346708f3d59797a2dd2cc2ba66194180
vihangwhite <75125449+vihangwhite@users.noreply.github.com>
2021-01-03 12:53:18 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,38 @@
+class Rope{
+    constructor(bodyA,pointB) {
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:1.2,
+            length:250
+
+        }
+       this.pointB = pointB;
+       this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+      }
+
+   attach(body){
+       this.rope.bodyA=body
+   }
+
+   fly(){
+       this.rope.bodyA=null;
+   }
+
+      display(){
+          if(this.rope.bodyA){
+
+          
+        var pointA= this.rope.bodyA.position;
+       var pointB = this.pointB;
+        push();
+        
+        strokeWeight(3)
+        stroke(48,22,8)
+        fill(255)
+        line( pointB.x,pointB.y, pointA.x,pointA.y);
+        pop();
+        }
+      }
+}
